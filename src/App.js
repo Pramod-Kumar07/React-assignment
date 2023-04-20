2d34b5e26a9d9026b2609ff41fe540dcf7d8f86e
batman <batman@gmail.com>
2023-04-21 01:14:45 +0530

    This is Invitation Card

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { Invitation } from './Component/Invitation';
 
 function App() {
+  const subject = "Birthaday party Invitation"
+  const to = "jaGdish@gmail.com"
+  const name1 = "jaGdish"
+  const birthdayParty = "birthday party"
+  const namesAll = " Ritu , Saurabh , Kartik ."
+  const GreenField = "Green field Avenue"
+  const name2 = "JaGdish"
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Invitation  subject={subject} to={to} name1={name1} birthdayParty={birthdayParty} namesAll={namesAll} GreenField={GreenField} name2={name2}/>
     </div>
   );
 }
