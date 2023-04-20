2dc266618571c01022623bfd37dc885719ed311b
Pramod Kumar <pro1625@gmail.com>
2023-04-20 11:03:32 +0530

    multiple components in root function

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import logo from './logo';
+import { Reactbenefit } from './reactbenefit';
+import button from './button'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h3>Benefits of React..</h3>
-        <ol>
-          <li>Component based architucture</li>
-          <li>Virtual DOM for efficient updates</li>
-          <li>Rich ecosyatem and comminity</li>
-          <li>Cross platform development</li>
-          <li>Strong community support</li>
-        </ol>
-        <butoon>Get Started..</butoon>
+       <logo/>
+       <Reactbenefit/>
+       <button/>
       </header>
     </div>
   );
---
+++ src/reactbenefit.js
@@ -0,0 +1,14 @@
+export function Reactbenefit (){
+    return(
+        <>
+            <h3>Benefits of React..</h3>
+            <ol>
+            <li>Component based architucture</li>
+            <li>Virtual DOM for efficient updates</li>
+            <li>Rich ecosystem and comminity</li>
+            <li>Cross platform development</li>
+            <li>Strong community support</li>
+            </ol>
+        </>
+    )
+}
