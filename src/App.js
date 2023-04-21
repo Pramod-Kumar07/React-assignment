1ad08ffbe3d6baf3631cf83a8dd0ef5ec59a80f0
Pramod Kumar <pro1625@gmail.com>
2023-04-21 14:20:07 +0530

    Invitation styled

--- src/App.js
+++ src/App.js
@@ -4,15 +4,15 @@ import Invitation from './component/invitation';
 
 function App() {
   return (
-    <div className="App">      
-      <Invitation 
-      invite="Birthday party Invitation"
+          
+      <Invitation
+      subject="Birthday party Invitation"
       email="jaGdish@gmail.com"
       name="jaGdish"
       title="birthday party"
       invitee="Ritu , Saurabh , Kartik"
       venue="Green field Avenue"/>      
-    </div>
+    
   );
 }
 
--- src/component/invitation.js
+++ src/component/invitation.js
@@ -1,23 +1,25 @@
-import React from 'react'
+import React from 'react';
+import './invitation.css'
+
 
 export default function Invitation(props){
     return(
-        <div>
-        <p>Subject : {props.invite}  
+        <div className='invitation'>
+        <p>Subject : <span>{props.subject}</span>
         <br/>              
-        To : {props.email}
+        To : <span>{props.email}</span>
         </p>
         
-        <p>Hi , {props.name}</p>
+        <p>Hi , <span>{props.name}</span></p>
         
-        <p> I am having a {props.title} next Friday at my Home . Would you like to come ? It will be fun . Lots of people from my school are coming . you know some of them - {props.invitee} .
+        <p> I am having a <span>{props.title} </span>next Friday at my Home . Would you like to come ? It will be fun . Lots of people from my school are coming . you know some of them -<span>{props.invitee}</span>.
         <br/><br/>
-        My house is behind our school , near {props.venue} .
+        My house is behind our school , near <span> {props.venue}</span>.
         <br/><br/>
         I hope you will come and see you soon .</p>    
         
         <p>from ,<br/>
-        {props.name}</p>
+        <span>{props.name}</span></p>
 
      </div>
     )
