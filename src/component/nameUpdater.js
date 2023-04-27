d8208356c811b6c3ee96f577afbfb9f39e73409f
Pramod Kumar <pro1625@gmail.com>
2023-04-27 09:31:37 +0530

    updated h1 heading

--- src/component/nameUpdater.js
+++ src/component/nameUpdater.js
@@ -12,7 +12,7 @@ export default function NameUpdater(){
         <div>
             <input type="text" value={input} placeholder="Add your name.."  onChange={(element)=>setInput(element.target.value)}/>
             <button onClick={handleClick}>Submit</button>
-            <h1>Hi, My name is :{update}{input}</h1>
+            <h1>Hi, My name is :{update}</h1>
         </div>
     )
 }
