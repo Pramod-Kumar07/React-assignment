74c353b478caf079740ba799dab81f31c06910b1
Pramod Kumar <pro1625@gmail.com>
2023-05-03 09:59:49 +0530

    removed import catch which was added by mistake

--- src/Component/DogData.js
+++ src/Component/DogData.js
@@ -1,6 +1,5 @@
 import { useState,useEffect } from "react";
 import './DogData.css';
-import { click } from "@testing-library/user-event/dist/click";
 
 function DogData() {
     const [image, setImage]=useState('https://images.unsplash.com/photo-1611003228941-98852ba62227?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YmFieSUyMGRvZ3xlbnwwfHwwfHw%3D&w=1000&q=80');
