a8ab3a801831a2932d17310acdedf543ee2641e6
Pramod Kumar <pro1625@gmail.com>
2023-05-24 21:57:27 +0530

    added styling

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import React from 'react';
+import styles from './Home.module.css';
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className={styles.home}>
+        <div>
+          <h1>Hi There!</h1>
+        </div>
+        <div>
+          <h1>Welcome...</h1>
+        </div>
+        <div>
+          <h1>I'M PRAMOD KUMAR</h1>
+        </div>
+        <div>
+          <h1>FRONT END DEVELOPER</h1>
+        </div>
+      </div>
   )
 }
 
