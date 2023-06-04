edc83e4f8d147a22a9283e5dfc5090113b258d1e
Pramod Kumar <pro1625@gmail.com>
2023-06-05 00:38:58 +0530

    googlr docs with functionality using quill library

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import styles from './App.module.css';
+import Navbar from './components/Navbar';
+import TextEditor from './components/TextEditor';
 
 function App() {
   return (
     <div className={styles.App}>
-      
+      <Navbar/>
+      <TextEditor/>
     </div>
   );
 }
