1466986e8685f254a499bab5affcc7215d04fd6a
Pramod <123134499+Pramod-Kumar07@users.noreply.github.com>
2023-06-10 14:29:02 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import styles from './App.module.css';
 import Navbar from './components/Navbar/Navbar';
-import TextField from './components/Navbar/TextField/TextField';
 
 function App() {
   return (
     <div className={styles.App}>
       <Navbar/>
-      {/* <TextField/> */}
     </div>
   );
 }
